094d26570372f2730fd6dfc91f891fc036fc6220
kindtiger <seonbeom2@gmail.com>
2025-03-15 14:01:15 +0900

    a

--- src/matter.js
+++ src/matter.js
@@ -25,31 +25,95 @@ let render = Render.create({
       height:innerHeight,
       background: '#222',
       wireframeBackground: '#222',
-      wireframes: true,
+      wireframes: false,
       // showAngleIndicator: true
 
     }
 });
 
+const category = [
+  {
+    content:'adventurous',
+    width:424,
+    height:83,
+    img:"/section01/adventurous.png",
+    x:0,
+    y:0
+  },
+  {
+    content:'bright',
+    width:239,
+    height:83,
+    img:"/section01/bright.png",
+    x:0,
+    y:0
+  },
+  {
+    content:'ENFJ',
+    width:224,
+    height:83,
+    img:"/section01/ENFJ.png",
+    x:0,
+    y:0
+  },
+  {
+    content:'flexible',
+    width:285,
+    height:83,
+    img:"/section01/flexible.png",
+    x:0,
+    y:0
+  },
+  {
+    content:'passionate',
+    width:371,
+    height:83,
+    img:"/section01/passionate.png",
+    x:0,
+    y:0
+  },
+]
+
+
+const arr = [];
 const xCenter = innerWidth/2;
 
-const img = new Image();
-img.src = '/section01/adventurous.png';
+category.forEach((item, i) => {
+  const img = new Image();
+  img.onload = function() {
 
-// create two boxes and a ground
-let boxA = Bodies.rectangle(400, 200, 80, 80);
-let boxB = Bodies.rectangle(xCenter, 50, 424, 83,{
-  chamfer: {
-    radius: [40, 40] // border-radius 설정이 가능합니다.
-  },
-  render:{
-    sprite:{
-      texture:img.src,
-      // xScale:1,
-      // yScale:1
-    }
-  }
+    const body = Bodies.rectangle(xCenter, 600, item.width, item.height, {
+      chamfer: {
+        radius: [40, 40],
+      },
+      
+      render: {
+        sprite: {
+          texture: this.src,
+        },
+      },
+    });
+    
+    // 각 Body를 개별적으로 추가
+    Composite.add(engine.world, body);
+    arr.push(body);
+
+    
+  };
+  img.src = item.img;
 });
+
+
+
+
+// const tags = category.map((item,i)=>{
+//   return Bodies.rectangle(xCenter, 600, 424, 83,{
+//     ...options,
+//     ...options.render.sprite.texture = createImage(i)
+//    });
+// })
+
+
 
 let ground = Bodies.rectangle(xCenter, 610, 810, 1, { isStatic: true });
 
@@ -97,7 +161,7 @@ render.mouse = mouse;
 
 
 // add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
+Composite.add(engine.world, [ ground]);
 
 // run the renderer
 Render.run(render);
